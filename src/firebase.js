bb012af7567f0c5d32fa884dc6fdc2b2b7f74c65
Kevin Caster <nyagucha.kevin.otwori@gmail.com>
2021-08-06 01:29:36 -0700

    final commit

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0-SmFBA27ba4_hklegTM9kqxOb-yJwHw",
-  authDomain: "todo-dacd5.firebaseapp.com",
-  projectId: "todo-dacd5",
-  storageBucket: "todo-dacd5.appspot.com",
-  messagingSenderId: "1059395913580",
-  appId: "1:1059395913580:web:c0959fa2998a949ccdd549",
+  apiKey: "YOUR API KEY",
+  authDomain: "YOUR AUTH DOMAIN",
+  projectId: "YOUR PROJECT-ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "",
+  appId: "YOUR APP ID",
 };
 
 // Initialize Firebase
